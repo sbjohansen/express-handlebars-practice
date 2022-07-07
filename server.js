c7a22c9a169df3939a9582af7493cdd6515025af
Slawomir Johansen <baran@fastmail.com>
2022-07-07 15:53:57 +0200

    ADD base structure/functionality

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/hello/:name', (req, res) => {
+    res.render('hello', { name: req.params.name });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+    res.render('info');
+});
+
+app.get('/history', (req, res) => {
+    res.render('history', { layout: 'dark' });
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 not found...');
+});
+
+app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
+});
