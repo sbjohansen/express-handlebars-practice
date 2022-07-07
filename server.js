af124b3aca4959c3a78855e99428af5b04c9f589
Slawomir Johansen <baran@fastmail.com>
2022-07-08 00:15:18 +0200

    ADD irlencoded middleware

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post('/contact/send-message', (req, res) => {
+    res.json(req.body);
+});
 
 app.get('/', (req, res) => {
     res.render('index');
